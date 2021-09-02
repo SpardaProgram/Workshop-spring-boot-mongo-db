2d444b20ae8993f83f96169a60902b2f03b4e427
Luiz Flávio <luizflavioandradecorrea@gmail.com>
2021-09-02 10:16:15 -0300

    Projeto Criado

---
+++ workshopmongo/src/test/java/com/spardaprogram/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spardaprogram.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
